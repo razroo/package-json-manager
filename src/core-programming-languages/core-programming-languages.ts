482cd3990a16960e367c7ebce7aa931a5db37ea6
Charlie Greenman <charlie@razroo.com>
2023-01-10 22:47:38 +0200

    Add a core programming languages map.

---
+++ src/core-programming-languages/core-programming-languages.ts
@@ -0,0 +1,6 @@
+export function coreProgrammingLanguagesMap() {
+  return {
+    '@angular/core': 'angular',
+    'react': 'react'
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 // export { Blah as default } from './lib'
+export { coreProgrammingLanguagesMap } from "./core-programming-languages/core-programming-languages";
