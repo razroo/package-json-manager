76b3128ea6bc78fb80c5e45497eb476f807d07b7
Charlie Greenman <charlie@razroo.com>
2023-01-12 00:58:04 +0200

    Fix padding for package json manager.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-
-
-// export { Blah as default } from './lib'
 export { coreProgrammingLanguagesMap } from "./core-programming-languages/core-programming-languages";
 export { readPackageJson, findPackageJson, determineLanguageWithVersionUsed, determineLanguagesUsed, getProjectDependencies } from "./core/package-json";
