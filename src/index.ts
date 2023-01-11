d0ffa70ffa6a7355ebc22e3f2c972a5f526cc5f0
Charlie Greenman <charlie@razroo.com>
2023-01-12 00:56:12 +0200

    Add proper exports for package json manager.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+
+
 // export { Blah as default } from './lib'
 export { coreProgrammingLanguagesMap } from "./core-programming-languages/core-programming-languages";
+export { readPackageJson, findPackageJson, determineLanguageWithVersionUsed, determineLanguagesUsed, getProjectDependencies } from "./core/package-json";
