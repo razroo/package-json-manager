381249e79a8eab580e0e446073eb9b7a70cba92d
Charlie Greenman <charlie@razroo.com>
2023-03-27 10:08:10 +0300

    Export PackageJson interface.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { coreProgrammingLanguagesMap } from "./core-programming-languages/core-programming-languages";
-export { readPackageJson, findPackageJson, searchForPackageJson, determineLanguagesWithVersionUsed, determineLanguagesUsed, getProjectDependencies, getAllDependencies } from "./core/package-json";
+export { readPackageJson, findPackageJson, searchForPackageJson, determineLanguagesWithVersionUsed, 
+    determineLanguagesUsed, getProjectDependencies, getAllDependencies, PackageJson } from "./core/package-json";
